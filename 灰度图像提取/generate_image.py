2048a601e8be3b5583688e220fbe2a25f8dfc088
yaolei <718229223@qq.com>
2017-05-05 10:07:42 +0800

    5.5 灰度图像提取update

--- 灰度图像提取/generate_image.py
+++ 灰度图像提取/generate_image.py
@@ -8,22 +8,6 @@ import os
 workspace = "./"
 tmp_workspace = os.path.join(workspace,"slice_image/")
 
-file_path = "data_sample.txt"
-radar_echos = []
-#这里取第三条数据为样本作出灰度图像
-i = 0
-with open(file_path,"r") as infile:
-    for line in infile:
-        i = i+1
-        if i==3:
-            context = line.split(",")
-            #print context[0]
-            radar_echos = context[2].split(" ")
-            #print len(radar_echos)
-            break
-
-radar_np = np.array(radar_echos[0:10201])
-
 #将np中的字符变为数字
 def parse_maybe_int(i):
     if i == '':
@@ -31,15 +15,28 @@ def parse_maybe_int(i):
     else:
         return int(i)
 
-new_radar_list = []
 
-for e in radar_np:
-    e = parse_maybe_int(e)
-    new_radar_list.append(e)
-#print new_radar_list[0:3]
-new_radar_np = np.array(new_radar_list)
-
-radar_map = new_radar_np.reshape(101,101)
-print len(radar_map)
-
-cv2.imwrite(os.path.join(tmp_workspace, "%04d_%04d_%04d_%s.jpg" % (0,0,0,1)),radar_map)
+file_path = "data_sample.txt"
+radar_echos = []
+#这里取第三条数据为样本作出灰度图像
+i = 0
+with open(file_path,"r") as infile:
+    for line in infile:
+        context = line.split(",")
+        print context[0]
+        radar_echos = context[2].split(" ")
+        i = 0
+        while i<612060:
+            radar_np = np.array(radar_echos[i:i+10201])
+            new_radar_list = []
+            for e in radar_np:
+                e = parse_maybe_int(e)
+                new_radar_list.append(e)
+            new_radar_np = np.array(new_radar_list)
+            radar_map = new_radar_np.reshape(101,101)
+            k = (i / 10201)
+            #print k/4
+            #print k%4
+            cv2.imwrite(os.path.join(tmp_workspace, "%s_%s_%s_%s.jpg" % (context[0],context[1],k/4,k%4)),radar_map)
+            i = i + 10201
+        break
